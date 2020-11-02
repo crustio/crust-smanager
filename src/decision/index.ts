9934a788a864c26e631c57a3550ca1d0ca8af7f4
Zikun Fan <zikunvan@gmail.com>
2020-11-02 21:08:06 +0800

    Add de base structure

---
+++ src/decision/index.ts
@@ -0,0 +1,10 @@
+export class DecisionEngine {
+  /**
+   * Pick or drop the given cid
+   * @param cid ipfs cid
+   */
+  pickOrDrop(cid: string): boolean {
+    // TODO: add strategy
+    return true;
+  }
+}
