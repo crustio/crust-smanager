e6dc718fa38fac92610a9dca2b49fb4b39dc323a
Kun <zikunvan@gmail.com>
2020-11-09 14:16:04 +0800

    Dockerize (#6)

---
+++ dockerize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+IMAGEID="crustio/crust-smanager:$PACKAGE_VERSION"
+echo "Building crustio/crust-smanager:$PACKAGE_VERSION ..."
+docker build -t $IMAGEID .
