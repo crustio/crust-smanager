535b5a90d2bcf01a6701a6f016f0ff74feb2b5a0
Zikun Fan <zikunvan@gmail.com>
2021-01-06 19:32:04 +0800

    Change decision with new market interface

--- src/decision/index.ts
+++ src/decision/index.ts
@@ -5,8 +5,9 @@ import BigNumber from 'bignumber.js';
 import {Header} from '@polkadot/types/interfaces';
 import TaskQueue, {BT} from '../queue';
 import IpfsApi from '../ipfs';
-import CrustApi, {StorageOrder} from '../chain';
+import CrustApi, {DetailFileInfo, FileInfo} from '../chain';
 import {logger} from '../log';
+import {hexToString} from '../util';
 
 // The initial probability is 5‰
 const initialProbability = 0.005;
@@ -21,8 +22,8 @@ interface Task extends BT {
 export default class DecisionEngine {
   private readonly crustApi: CrustApi;
   private readonly ipfsApi: IpfsApi;
-  private pendingQueue: TaskQueue<Task>;
   private pullingQueue: TaskQueue<Task>;
+  private sealingQueue: TaskQueue<Task>;
   private currentBn: number;
 
   constructor(chainAddr: string, ipfsAddr: string, mto: number) {
@@ -30,15 +31,16 @@ export default class DecisionEngine {
     this.ipfsApi = new IpfsApi(ipfsAddr, mto);
 
     // MaxQueueLength is 50 and Overdue is 600 blocks(1h)
-    this.pendingQueue = new TaskQueue<Task>(50, 600);
-    this.pullingQueue = new TaskQueue<Task>(30, 600);
+    this.pullingQueue = new TaskQueue<Task>(50, 600);
+    this.sealingQueue = new TaskQueue<Task>(30, 600);
 
     // Init the current block number
+    // TODO: Do the restart mechanism
     this.currentBn = 0;
   }
 
   /**
-   * Subscribe new storage order extrinsic, scheduling by `subscribeNewHeads`
+   * Subscribe new file order extrinsic, scheduling by `subscribeNewHeads`
    * It will also check the outdated tasks
    * @returns stop `taking new storage order`
    * @throws crustApi error
@@ -47,31 +49,32 @@ export default class DecisionEngine {
     const addPendings = async (b: Header) => {
       // 1. Get block number
       const bn = b.number.toNumber();
-      logger.info(`⛓  Got new block ${bn} from chain`);
+      const bh = b.hash.toString();
+      logger.info(`⛓  Got new block ${bn}(${bh})`);
       // 2. Update current block number
       this.currentBn = bn;
       // 3. Try to get new storage order
-      const newSorder: StorageOrder | null = await this.crustApi.maybeGetNewSorder(
-        bn
+      const newFile: FileInfo | null = await this.crustApi.parseNewFileByBlock(
+        bh
       );
       // 4. If got new storage order, put it into pendingQueue
-      if (newSorder) {
+      if (newFile) {
         const nt: Task = {
-          cid: newSorder.file_identifier,
+          cid: hexToString(newFile.cid),
           bn: bn,
-          size: _.parseInt(newSorder.file_size.replace(/,/g, '')),
+          size: newFile.size,
         };
         logger.info(
-          `  ↪ 🎁  Found new storage order, adding to pending queue ${JSON.stringify(
+          `  ↪ 🎁  Found new file, adding it to pending queue ${JSON.stringify(
             nt
           )}`
         );
         // Always push into pending queue
-        this.pendingQueue.push(nt);
+        this.pullingQueue.push(nt);
       }
       // 5. Check and clean outdated tasks
-      this.pendingQueue.clear(bn);
       this.pullingQueue.clear(bn);
+      this.sealingQueue.clear(bn);
     };
 
     const unsubscribe = await this.crustApi.subscribeNewHeads(addPendings);
@@ -87,33 +90,42 @@ export default class DecisionEngine {
   async subscribePullings(): Promise<cron.ScheduledTask> {
     return cron.schedule('* * * * *', async () => {
       // 1. Loop and pop all pending tasks
-      const oldPts: Task[] = this.pendingQueue.tasks;
+      const oldPts: Task[] = this.pullingQueue.tasks;
       const newPts = new Array<Task>();
-      logger.info('⏳  Checking pending queue...');
+      logger.info('⏳  Checking pulling queue ...');
+      logger.info(`  ↪ 🏃🏼‍♂️  Pulling queue length: ${oldPts.length}`);
 
       for (const pt of oldPts) {
         // 2. If join pullings and start puling in ipfs, otherwise push back to pending tasks
-        if (await this.pickOrDropPending(pt)) {
+        if (await this.pickOrDropPulling(pt)) {
           logger.info(
-            `  ↪ 🎁  Pick pending task ${JSON.stringify(pt)}, pulling from ipfs`
+            `  ↪ 🎁  Pick pulling task ${JSON.stringify(pt)}, pulling from ipfs`
           );
           // Async pulling
-          this.ipfsApi.pin(pt.cid).then(pinRst => {
-            if (!pinRst) {
-              // TODO: Maybe push back to pending queue?
-              logger.error(`  ↪ 💥  Pin ${pt.cid} failed`);
-            } else {
-              // Pin successfully
-              logger.info(`  ↪ ✨  Pin ${pt.cid} successfully`);
-            }
-          });
-          this.pullingQueue.push(pt);
+          this.ipfsApi
+            .pin(pt.cid)
+            .then(pinRst => {
+              if (!pinRst) {
+                // a. TODO: Maybe push back to pulling queue?
+                logger.error(`  ↪ 💥  Pin ${pt.cid} failed`);
+              } else {
+                // b. Pin successfully, add into sealing queue
+                logger.info(`  ↪ ✨  Pin ${pt.cid} successfully`);
+                this.sealingQueue.push(pt);
+              }
+            })
+            .catch(err => {
+              // c. Just drop it as 💩
+              logger.error(`  ↪ 💥  Pin ${pt.cid} failed with ${err}`);
+            });
         } else {
+          // d. Push back to pulling queue
           newPts.push(pt);
         }
       }
-      // 3. Set back to pending queue
-      this.pendingQueue.tasks = newPts;
+
+      // 3. Send errors back to pulling queue
+      this.pullingQueue.tasks = newPts;
     });
   }
 
@@ -125,13 +137,14 @@ export default class DecisionEngine {
   async subscribeSealings(): Promise<cron.ScheduledTask> {
     return cron.schedule('* * * * *', async () => {
       // 1. Loop pulling tasks
-      const oldPts: Task[] = this.pullingQueue.tasks;
+      const oldPts: Task[] = this.sealingQueue.tasks;
       const newPts = new Array<Task>();
-      logger.info('⏳  Checking pulling queue...');
+      logger.info('⏳  Checking sealing queue...');
+      logger.info(`  ↪ 🏃🏼‍♂️  Sealing queue length: ${oldPts.length}`);
 
       for (const pt of oldPts) {
         // 2. Judge if pulling successful, otherwise push back to pulling tasks
-        if (await this.pickOrDropPulling(pt.cid, pt.size)) {
+        if (await this.pickOrDropSealing(pt.cid, pt.size)) {
           // TODO: Call `sWorker.seal(pt.cid)` here
           logger.info(`  ↪ ⚙️  Send sWorker to seal: ${JSON.stringify(pt)}`);
         } else {
@@ -139,7 +152,7 @@ export default class DecisionEngine {
         }
       }
       // 3. Set back to pulling queue
-      this.pullingQueue.tasks = newPts;
+      this.sealingQueue.tasks = newPts;
     });
   }
 
@@ -148,39 +161,45 @@ export default class DecisionEngine {
   /// 1. random add storage orders;
   /// 2. judge file size and free space from local ipfs repo;
   /**
-   * Add or ignore to pending queue by a given cid
+   * Add or ignore to pulling queue by a given cid
    * @param cid ipfs cid value
    * @param f_size truly file size
-   * @throws ipfsApi error
+   * @returns if can pick
    */
-  // TODO: add pending pick up strategy here, basically random with pks?
-  private async pickOrDropPending(t: Task): Promise<boolean> {
-    // 1. Get and judge file size is match
-    const size = await this.ipfsApi.size(t.cid);
-    logger.info(`  ↪ 📂  Got ipfs file size ${t.cid}, size is: ${size}`);
-    if (size !== t.size) {
-      logger.warn(`  ↪ ⚠️  Size not match: ${size} != ${t.size}`);
+  // TODO: add pulling pick up strategy here, basically random with pks?
+  private async pickOrDropPulling(t: Task): Promise<boolean> {
+    try {
+      // 1. Get and judge file size is match
+      const size = await this.ipfsApi.size(t.cid);
+      logger.info(`  ↪ 📂  Got ipfs file size ${t.cid}, size is: ${size}`);
+      if (size !== t.size) {
+        logger.warn(`  ↪ ⚠️  Size not match: ${size} != ${t.size}`);
+        return true;
+      }
+
+      // 2. Get and judge repo can take it, make sure the free can take double file
+      // TODO: Remove this, cause this is no fucking use
+      const free = await this.ipfsApi.free();
+      const bn_f_size = new BigNumber(t.size);
+      if (free <= bn_f_size.multipliedBy(2)) {
+        logger.warn(`  ↪ ⚠️  Free space not enought ${free} < ${size}*2`);
+        return false;
+      }
+
+      // 3. Judge if it already been taked on chain or shoot it by chance
+      return this.isPulled(t.cid, t.bn);
+    } catch (err) {
+      logger.error(`  ↪ 💥  Access ipfs error, detail with ${err}`);
       return false;
     }
-
-    // 2. Get and judge repo can take it, make sure the free can take double file
-    // TODO: Remove this, cause this is no fucking use
-    const free = await this.ipfsApi.free();
-    const bn_f_size = new BigNumber(t.size);
-    if (free <= bn_f_size.multipliedBy(2)) {
-      logger.warn(`  ↪ ⚠️  Free space not enought ${free} < ${size}*2`);
-      return false;
-    }
-
-    return this.isFileOnChain(t.cid, t.bn);
   }
 
   /**
-   * Pick or drop pulling queue by a given cid
+   * Pick or drop sealing queue by a given cid
    * @param cid ipfs cid value
    * @param f_size truly file size
    */
-  private async pickOrDropPulling(
+  private async pickOrDropSealing(
     _cid: string,
     _f_size: number
   ): Promise<boolean> {
@@ -195,19 +214,31 @@ export default class DecisionEngine {
    * @param bn task block number
    * @throws crustApi error
    */
-  private async isFileOnChain(_cid: string, bn: number): Promise<boolean> {
-    // TODO: const fileInfo = await this.crustApi.maybeGetFile(cid);
-    // TODO: judge if fileInfo.expected_payouts < len(fileInfo.payouts), if true, return false
-    // TODO: if false, calculate probability with `expired_date`
+  private async isPulled(cid: string, bn: number): Promise<boolean> {
+    // TODO: Set flag to let user choose enable the `only take order file`
+    const fileInfo: DetailFileInfo | null = await this.crustApi.maybeGetNewFile(
+      cid
+    );
+    // If replicas already reach the limit
+    if (
+      fileInfo &&
+      fileInfo?.replicas.length > Number(fileInfo.expected_replica_count)
+    ) {
+      return false;
+    }
+
+    // else, calculate the probability with `expired_date`
 
     // 1. Generate a number between 0 and 1
     const randNum = Math.random();
+
     // 2. Calculate probability
     const multiple = (this.currentBn - bn) / 1 + 1; // 1 unit means 1min
     const probability = initialProbability * multiple; // probability will turns into 100% after 200 * 1 unit = 200min
     logger.info(
-      `💓  Current randNum is ${randNum}, New target is ${probability}, ${this.currentBn}, ${bn}, ${multiple}`
+      `💓  Current randNum is ${randNum}, New target is ${probability}, Current block is ${this.currentBn}, Task block is ${bn}`
     );
+
     // 3. Judge if we hit the spot
     return randNum < probability;
   }
