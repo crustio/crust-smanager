0479189a285d63d5fcc5f5605692b2a11ed6d4e0
Zikun Fan <zikunvan@gmail.com>
2021-02-20 14:41:48 +0800

    Apply crust.js

--- src/chain/index.ts
+++ src/chain/index.ts
@@ -3,84 +3,16 @@ import {ApiPromise, WsProvider} from '@polkadot/api';
 import {Header, Extrinsic, EventRecord} from '@polkadot/types/interfaces';
 import {logger} from '../log';
 import {parseObj, sleep} from '../util';
-
-// TODO: use crust.js types instead of this savage way
-const types = {
-  Address: 'AccountId',
-  AddressInfo: 'Vec<u8>',
-  ETHAddress: 'Vec<u8>',
-  EthereumTxHash: 'H256',
-  FileAlias: 'Vec<u8>',
-  FileInfo: {
-    file_size: 'u64',
-    expired_on: 'BlockNumber',
-    claimed_at: 'BlockNumber',
-    amount: 'Balance',
-    expected_replica_count: 'u32',
-    reported_replica_count: 'u32',
-    replicas: 'Vec<Replica<AccountId>>',
-  },
-  Guarantee: {
-    targets: 'Vec<IndividualExposure<AccountId, Balance>>',
-    total: 'Compact<Balance>',
-    submitted_in: 'EraIndex',
-    suppressed: 'bool',
-  },
-  IASSig: 'Vec<u8>',
-  Identity: {
-    anchor: 'SworkerAnchor',
-    group: 'Option<AccountId>',
-  },
-  ISVBody: 'Vec<u8>',
-  LookupSource: 'AccountId',
-  MerchantLedger: {
-    reward: 'Balance',
-    pledge: 'Balance',
-  },
-  MerkleRoot: 'Vec<u8>',
-  ReportSlot: 'u64',
-  Replica: {
-    who: 'AccountId',
-    valid_at: 'BlockNumber',
-    anchor: 'SworkerAnchor',
-  },
-  Releases: {
-    _enum: ['V1_0_0', 'V2_0_0'],
-  },
-  PKInfo: {
-    code: 'SworkerCode',
-    allow_report_slot: 'ReportSlot',
-    anchor: 'Option<SworkerAnchor>',
-  },
-  Status: {
-    _enum: ['Free', 'Reserved'],
-  },
-  SworkerAnchor: 'Vec<u8>',
-  SworkerCert: 'Vec<u8>',
-  SworkerCode: 'Vec<u8>',
-  SworkerPubKey: 'Vec<u8>',
-  SworkerSignature: 'Vec<u8>',
-  UsedInfo: {
-    used_size: 'u64',
-    reported_group_count: 'u32',
-    groups: 'BTreeMap<SworkerAnchor, bool>',
-  },
-  WorkReport: {
-    report_slot: 'u64',
-    used: 'u64',
-    free: 'u64',
-    reported_files_size: 'u64',
-    reported_srd_root: 'MerkleRoot',
-    reported_files_root: 'MerkleRoot',
-  },
-};
+import {typesBundleForPolkadot, crustTypes} from '@crustio/type-definitions';
 
 export interface FileInfo {
   cid: string;
   size: number;
 }
 
-export type UsedInfo = typeof types.UsedInfo;
+export type UsedInfo = typeof crustTypes.market.types.UsedInfo;
+
+logger.info(`shenmene: ${JSON.stringify(crustTypes.market.types.UsedInfo)}`);
 
 export default class CrustApi {
   private readonly api: ApiPromise;
@@ -89,7 +21,7 @@ export default class CrustApi {
   constructor(addr: string, chainAccount: string) {
     this.api = new ApiPromise({
       provider: new WsProvider(addr),
-      types,
+      typesBundle: typesBundleForPolkadot,
     });
     this.chainAccount = chainAccount;
   }
