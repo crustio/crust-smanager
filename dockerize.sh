fffd0177122ad0ec3bcf4f94a1d51a37ea79751f
Haibin Lee <haibin.lee@foxmail.com>
2021-02-23 18:03:31 +0800

    Update dockerize.sh
    Optimized shell script for dockerization. Include 'build', 'run', 'stop' and 'restart' functions.

--- dockerize.sh
+++ dockerize.sh
@@ -7,5 +7,28 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 IMAGEID="crustio/crust-smanager:$PACKAGE_VERSION"
-echo "Building crustio/crust-smanager:$PACKAGE_VERSION ..."
-docker build -t $IMAGEID .
+DOCKER_RUN_OPTS="-d --rm --network=host --restart=always -e TZ=Asia/Shanghai --name=crust-smanager"
+
+case $1 in
+  build )
+    echo "Building $IMAGEID ..."
+    docker build --network=host -t $IMAGEID .
+    ;;
+  run )
+    echo "Starting $IMAGEID ..."
+    docker run $DOCKER_RUN_OPTS $IMAGEID
+    ;;
+  stop )
+    docker stop crust-smanager
+    ;;
+  restart )
+    docker stop crust-smanager
+    docker run $DOCKER_RUN_OPTS $IMAGEID
+    ;;
+  * )
+    echo "USAGE:"
+    echo "$0 build      build the '$IMAGEID' image"
+    echo "$0 run        start the 'crust-smanager' container"
+    echo "$0 stop       stop and remove the 'crust-smanager' container"
+    echo "$0 restart    restart the 'crust-smanager' container"
+esac
