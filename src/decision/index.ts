cc7548836146629df46d94636921faff68e42e83
Yang Zhao <jszyyx@163.com>
2021-06-18 18:01:32 +0800

    fix error

--- src/decision/index.ts
+++ src/decision/index.ts
@@ -203,6 +203,7 @@ export default class DecisionEngine {
             // Q length > 10 drop it to failed pts
             if (this.ipfsTaskCount > IPFSQueueLength) {
               failedPts.push(pt);
+              continue
             } else {
               this.ipfsTaskCount++;
             }
