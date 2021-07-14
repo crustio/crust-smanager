2261f71fb9534063fb248b6885b9737278f0da95
pangwa <pangwa@gmail.com>
2021-07-14 10:22:24 +0800

    using child logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 import { createLogger, format, transports } from 'winston';
 
-export const logger = createLogger({
+const defaultLogger = createLogger({
   level: 'info',
   format: format.combine(
     format.timestamp({
@@ -8,9 +8,17 @@ export const logger = createLogger({
     }),
     format.colorize(),
     format.errors({ stack: true }),
-    format.printf(
-      (info) => `[${info.timestamp}] ${info.level}: ${info.message}`,
-    ),
+    format.splat(),
+    format.printf((info) => {
+      let left = `[${info.timestamp}] ${info.level}: `;
+      if (info.modulePrefix) {
+        left += info.modulePrefix + ' ';
+      }
+      if (info.moduleId) {
+        left += `[${info.moduleId}]`;
+      }
+      return `${left} ${info.message}`;
+    }),
   ),
   transports: [
     //
@@ -21,4 +29,18 @@ export const logger = createLogger({
     new transports.File({ filename: 'crust-api-error.log', level: 'error' }),
     new transports.File({ filename: 'crust-api-combined.log' }),
   ],
+});
+
+export interface ChildLoggerConfig {
+  moduleId: string;
+  modulePrefix?: string;
+}
+
+export function createChildLogger(config: ChildLoggerConfig) {
+  return defaultLogger.child(config);
+}
+
+export const logger = createChildLogger({
+  moduleId: 'global',
+  modulePrefix: '☄',
 });
