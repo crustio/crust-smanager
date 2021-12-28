d580015a6fce46dfd452f1c4dc96f01d4ce74d75
LowEntropyBody <jszyyx@163.com>
2021-12-28 15:29:25 +0800

    update db

--- src/http/http-interface.ts
+++ src/http/http-interface.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Logger } from 'winston';
 import { AppContext } from '../types/context';
 import * as bodyParser from 'body-parser';
+import { FileInfo } from '../chain';
+import { createFileOrderOperator } from '../db/file-record';
 
 export async function startHttp(
     context: AppContext,
@@ -10,6 +12,7 @@ export async function startHttp(
     const logger = loggerParent.child({ moduleId: "http" });
     const app = express();
     const PORT = 42087;
+    const fileOrderOp = createFileOrderOperator(context.database);
 
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
@@ -29,7 +32,18 @@ export async function startHttp(
             return res.status(400).send('Please provide ordered cid');
         }
 
-        return res.status(200).send('Success');
+        const sfi: FileInfo[] = [{
+            cid: cid,
+            size: Number(fi.file_size),
+            tips: 0,
+            expiredAt: fi.expired_at ? Number(fi.expired_at) : null,
+            replicas: fi.replicas ? Number(fi.replicas) : null,
+            owner: null
+        }];
+
+        await fileOrderOp.addFiles(sfi, 'active', true);
+
+        return res.status(200).send(`Insert job: ${cid} success`);
     });
 
     app.listen(PORT, () => {
