0b1c36ed2755d1ab4b16915ec9a276f430d0f5d9
LowEntropyBody <jszyyx@163.com>
2021-12-29 13:57:22 +0800

    err catch

--- src/http/http-interface.ts
+++ src/http/http-interface.ts
@@ -24,38 +24,41 @@ export async function startHttp(
     logger.info("Configure smanager interface");
 
     app.post('/api/v0/insert', async (req, res) => {
-        const cid = req.body['cid'];
-        if (!cid) {
-            return res.status(400).send('please provide cid in the request');
+        try {
+            const cid = req.body['cid'];
+            if (!cid) {
+                return res.status(400).send('please provide cid in the request');
+            }
+            logger.info("try to insert wanted file: %s", cid);
+
+            const file: any = await context.api.chainApi().query.market.files(cid); // eslint-disable-line
+            if (file.isEmpty) {
+                logger.warn('wanted file %s not exist on chain', cid);
+                return res.status(400).send(`wanted file ${cid} not exist on chain`);
+            }
+
+            const fi = file.toJSON() as any; // eslint-disable-line
+            const fileInfo = {
+                ...fi,
+                amount: new BigNumber(fi.amount.toString()),
+            } as ChainFileInfo;
+
+            logger.info(`wanted file chain info: ${JSON.stringify(fileInfo)}`);
+
+            const sfi: FileInfo[] = [{
+                cid,
+                size: bytesToMb(fileInfo.file_size),
+                tips: fileInfo.amount.toNumber(),
+                owner: null,
+                replicas: fileInfo.reported_replica_count,
+                expiredAt: fileInfo.expired_at,
+            }];
+
+            await fileOrderOp.addFiles(sfi, 'wanted', true);
+            return res.status(200).send(`insert wanted file: ${cid} success`);
+        } catch (e) {
+            return res.status(500).send("internal server error");
         }
-        logger.info("try to insert wanted file: %s", cid);
-
-        const file: any = await context.api.chainApi().query.market.files(cid); // eslint-disable-line
-        if (file.isEmpty) {
-            logger.warn('wanted file %s not exist on chain', cid);
-            return res.status(400).send(`wanted file ${cid} not exist on chain`);
-        }
-
-        const fi = file.toJSON() as any; // eslint-disable-line
-        const fileInfo = {
-            ...fi,
-            amount: new BigNumber(fi.amount.toString()),
-        } as ChainFileInfo;
-        
-        logger.info(`wanted file chain info: ${JSON.stringify(fileInfo)}`);
-
-        const sfi: FileInfo[] = [{
-            cid,
-            size: bytesToMb(fileInfo.file_size),
-            tips: fileInfo.amount.toNumber(),
-            owner: null,
-            replicas: fileInfo.reported_replica_count,
-            expiredAt: fileInfo.expired_at,
-        }];
-
-        await fileOrderOp.addFiles(sfi, 'wanted', true);
-
-        return res.status(200).send(`insert wanted file: ${cid} success`);
     });
 
     app.listen(PORT, () => {
